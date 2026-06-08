ca0398bfdfc4dde766cb6c145c46cdc074e55447
unknown <alex.pop02@e-uvt.ro>
2026-06-08 13:36:46 +0300

    Added Card model and the test for it.

---
+++ Blackjack.Tests/CardTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using TheAdventure.Models; 
+
+namespace Blackjack.Tests;
+
+public class CardTests
+{
+    [Fact]
+    public void Card_ShouldHaveCorrectSuitAndValue()
+    {
+        var card = new Card(Suit.Hearts, CardValue.Ten);
+
+        Assert.Equal(Suit.Hearts, card.Suit);
+        Assert.Equal(CardValue.Ten, card.Value);
+    }
+}
